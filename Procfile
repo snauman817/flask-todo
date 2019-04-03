6ea8b071aa25be9943218089c4fd853443ffd915
Spencer Nauman <snauman817@gmail.com>
2019-04-03 15:57:23 -0400

    gunicorn wsgi set up with Procfile and wsgi.py

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn gettingstarted.wsgi --log-file -
+web: gunicorn flask_todo.wsgi --log-file -
---
+++ flask_todo/wsgi.py
@@ -0,0 +1,12 @@
+"""
+WSGI config for gettingstarted project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+from . import create_app
+
+application = create_app()
